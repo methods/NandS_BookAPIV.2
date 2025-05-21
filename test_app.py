8bd072f0399498edad13f977e07fb785a442d0d8
Nicholas Goh <Nicholas.Goh@methods.co.uk>
2025-05-21 15:33:49 +0100

    add pytest fixtures and initial POST test to test_app.py

--- test_app.py
+++ test_app.py
@@ -1,15 +1,39 @@
+
 from app import app
+import pytest
+import uuid
+import json
 
+@pytest.fixture
+def random_uuid():
+    return str(uuid.uuid4())
 
-def test_helloworld_endpoint_returns_expected_results():
-
+@pytest.fixture
+def client():
     app.config['TESTING'] = True
-    client = app.test_client()
+    return app.test_client()
 
+def test_helloworld_endpoint_returns_expected_results(client):
     response = client.get("/helloworld")
 
     assert response.status_code == 200
     assert "text" in response.content_type
     assert response.data.decode() == "hello world"
 
+def test_add_book_creates_new_book(client, random_uuid):
 
+    test_book = {
+        "id": str(random_uuid),
+        "title": "Test Book",
+        "author": "AN Other",
+        "synopsis": "Test Synopsis",
+        "links": {
+            "self": f'/books/{id}',
+            "reservations": f'/books/{id}/reservations',
+            "reviews": f'/books/{id}/reviews'
+        }
+    }
+
+    response = client.post("/books", json = test_book)
+
+    assert response.status_code == 201
