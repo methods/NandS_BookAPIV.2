786068bd85919a8c54fe6f4822c47ecad4a2e942
Nicholas.Goh@methods.co.uk <Nicholas.Goh@methods.co.uk>
2025-06-19 17:20:56 +0100

    Add error handling for missing .env, sample data file, mongoDB to create_resources.sh

--- scripts/create_resources.sh
+++ scripts/create_resources.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-mongoimport --db BOOKS_API --collection books --file test_data/sample_books.json --jsonArray
+# Load environment variables for the mongoDB from .env in project root
+if [ -f "../.env" ]; then
+  source "../.env"
+else
+  echo "Error: .env file not found in project root directory."
+  exit 1
+fi
+
+# Check the sample data file exists
+if [ ! -f "test_data/sample_books.json" ]; then
+  echo "Error: sample_books.json file not found in test_data directory."
+  exit 1
+fi
+
+# Check if MongoDB is running
+if ! mongosh --eval "db.adminCommand('ping')" >/dev/null 2>&1; then
+    echo "Error: MongoDB is not running."
+    exit 1
+fi
+
+# Import the sample data into mongoDB
+if mongoimport --db "$PROJECT_DATABASE" --collection "$PROJECT_COLLECTION" --file test_data/sample_books.json --jsonArray; then
+  echo "Database '$PROJECT_DATABASE', collection '$PROJECT_COLLECTION' populated successfully."
+else
+  echo "Error: Failed to populate the database."
+    exit 1
+fi
